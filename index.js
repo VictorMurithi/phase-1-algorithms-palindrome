4d06d26b73a0b6a7b767cbf0086a16c1e46b660f
victor <victor.murithi@student.moringaschool.com>
2023-10-04 13:12:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const lowerCaseWord = word.toLowerCase();
+  const reversedWord = lowerCaseWord.split('').reverse().join('')
+  if(lowerCaseWord === reversedWord){
+    return 'true'
+  }else{
+    return 'false'
+  }
 }
 
 /* 
   Add your pseudocode here
+  *First write the function isPalindrome()
+  *Then create a variable that takes the word and changes it to lowerCase
+  *Then create a variable that changes the lowercasedword to reversed
+  *Then create a if condition that checks if the lowercasedword === the variable in reverse 
+  *we turn the variable in reverse by using split() which splits the words
+  *we use reverse () to reverse the words
+  *we use join () to join the reversed words
 */
 
 /*
   Add written explanation of your solution here
+ *The function takes in a string(word) then it changes the word to lowercase and assigns it to a variable
+ *The function changes the lowercased word to a reversed word and assigns it to a variable
+ *The function then compares the two variables if they are the same
+ *If they are , it returns true , if they are not it returns false
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +33,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting : true");
+  console.log("=>", isPalindrome("abba"))
+
+  console.log("expecting: true")
+  console.log("=>", isPalindrome("a"))
+
+  console.log("Expecting : false")
+  console.log("=>", isPalindrome("ab"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
